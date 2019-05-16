484a58715f451dfb9ffae19854b568284f7b236c
Alex <100306255@alumnos.uc3m.es>
2019-05-16 12:23:40 +0200

    Statistics activity extraccion de medias y desv tipicas a archivo de texto

--- app/src/main/java/com/alex/deu/StatisticsActivity.java
+++ app/src/main/java/com/alex/deu/StatisticsActivity.java
@@ -8,9 +8,14 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import static java.lang.Math.pow;
 import static java.lang.StrictMath.sqrt;
@@ -25,20 +30,20 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
     //20.000 microsegundos -> 50 muestras/segundo
     // Para 10000 microseconds -> 100 muestras/segundo
 
-    private double intervalo = 4;// 2 segundos
-    private double delay_seg = (double) delay / 1000000;
-    private double muestras_seg = 1 / ((double) delay_seg);
-    private int data_size = (int) (intervalo * muestras_seg);//Tamaño del array para calcular estadisticos
+
+    private int data_size;//Tamaño del array para calcular estadisticos
 
     private TextView tv_med, tv_std, tv;
 
     // guarda los valores x, y, z cada ez que cambia el sensor grav
-    private float[] gravityValues = new float[3];
-    private double[] array_az = new double[data_size];
-    private int az_index = 0;
+    private float[] gravityValues;
+    private double[] array_az;
+    private int az_index;
 
-    private double[] array_gz = new double[data_size];
-    private int gz_index = 0;
+    private double[] array_gz;
+    private int gz_index;
+
+    private ArrayList<Double> a_medias, a_desv_tipicas, g_medias, g_desv_tipicas;
 
 
     /**
@@ -62,38 +67,92 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
     /**
      * Al iniciar activity regustrar escuchadores de sensores
      */
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public void startStatistics(View view) {
         if (mAcce != null) {
             sensorManager.registerListener(this, mAcce, delay);
-            Log.d(TAG, "onResume: Registrando el sensor acelerometro");
+            Log.d(TAG, "START: Registrando el sensor acelerometro");
         } else {
             tv.setText(tv.getText().toString() + "\nAcc no disponible");
         }
         if (mGrav != null) {
             sensorManager.registerListener(this, mGrav, delay);
-            Log.d(TAG, "onResume: Registrando el sensor gravedad");
+            Log.d(TAG, "START: Registrando el sensor gravedad");
         } else {
             tv.setText(tv.getText().toString() + "\nGrav no disponible");
         }
         if (mGyro != null) {
             sensorManager.registerListener(this, mGyro, delay);
-            Log.d(TAG, "onResume: Registrando el sensor giroscopio");
+            Log.d(TAG, "START: Registrando el sensor giroscopio");
         } else {
             tv.setText(tv.getText().toString() + "\nGyro no disponible");
         }
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //delay = 20000;
+        //20.000 microsegundos -> 50 muestras/segundo
+        double intervalo = 4;//segundos
+        double delay_seg = (double) delay / 1000000;//pasar de microsegundos a segundos
+        double muestras_seg = 1 / ((double) delay_seg);
+        data_size = (int) (intervalo * muestras_seg);//Tamaño del array para calcular estadisticos
+
+
+        // guarda los valores x, y, z cada ez que cambia el sensor grav
+        gravityValues = new float[3];
+        array_az = new double[data_size];
+        az_index = 0;
+
+        array_gz = new double[data_size];
+        gz_index = 0;
+
+        a_medias = new ArrayList<Double>();
+        a_desv_tipicas = new ArrayList<Double>();
+        g_medias = new ArrayList<Double>();
+        g_desv_tipicas = new ArrayList<Double>();
+    }
+
     /**
      * Apagar escuchadores
      */
+    public void stopStatistics(View view) {
+        sensorManager.unregisterListener(this, mGrav);
+        sensorManager.unregisterListener(this, mAcce);
+        sensorManager.unregisterListener(this, mGyro);
+        Log.d(TAG, "STOP: Unregistering sensor listeners");
+
+
+        makeStatisticsFiles();
+
+
+
+        /*
+         * Resetear variables
+         * */
+        gravityValues = new float[3];
+        array_az = new double[data_size];
+        az_index = 0;
+
+        array_gz = new double[data_size];
+        gz_index = 0;
+
+        a_medias = new ArrayList<Double>();
+        a_desv_tipicas = new ArrayList<Double>();
+        g_medias = new ArrayList<Double>();
+        g_desv_tipicas = new ArrayList<Double>();
+    }
+
+
     @Override
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this, mGrav);
         sensorManager.unregisterListener(this, mAcce);
         sensorManager.unregisterListener(this, mGyro);
+        Log.d(TAG, "onPause: Unregistering sensor listeners");
+
     }
 
     @Override
@@ -157,6 +216,9 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
                     Log.d(TAG, "Desviacion Tipica ACC: " + desv);
                     array_az = new double[data_size];
                     az_index = 0;
+
+                    a_medias.add(med);
+                    a_desv_tipicas.add(desv);
                 }
                 break;
 
@@ -192,6 +254,10 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
                     Log.d(TAG, "Desviacion Tipica GYRO: " + desv);
                     array_gz = new double[data_size];
                     gz_index = 0;
+
+                    g_medias.add(med);
+                    g_desv_tipicas.add(desv);
+
                 }
                 break;
 
@@ -243,7 +309,7 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
             // devuelve la raiz cuadrada de la varianza, es decir, la desviacion tipica
             return sqrt(varianza);
         } else {
-            // si la media no se pasa como parametro se calcula aqui
+            // si la media no se pasa como 0 se calcula aqui
             med = media(data);
             while (i < data.length) {
                 sum += pow(data[i] - med, 2);
@@ -255,7 +321,7 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
         }
     }
 
-    public double mediana(double[] data){
+    public double mediana(double[] data) {
 
         return 0;
     }
@@ -275,4 +341,56 @@ public class StatisticsActivity extends AppCompatActivity implements SensorEvent
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
+
+
+    public void makeStatisticsFiles() {
+        Log.d(TAG, "makeStatisticsFiles: a_medias.size = " + a_medias.size() +
+                ", a_desv_tipicas.size = " + a_desv_tipicas.size());
+        Log.d(TAG, "makeStatisticsFiles: g_medias.size = " + g_medias.size() +
+                ", g_desv_tipicas.size = " + g_desv_tipicas.size());
+        //Log.d(TAG, "MEDIAS: " + a_medias.toString());
+        //Log.d(TAG, "DESV TIPICAS: " + a_desv_tipicas.toString());
+
+        Calendar c = Calendar.getInstance();
+        int sec = c.get(Calendar.SECOND);
+        int min = c.get(Calendar.MINUTE);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = 1 + c.get(Calendar.MONTH);
+        int year = c.get(Calendar.YEAR);
+        String date = year + "-" + month + "-" + day + "_" + hour + "-" + min + "-" + sec;
+        String a_m, a_d, g_m, g_d;
+        a_m = "a_med_"+date+".txt";
+        g_m = "g_med_"+date+".txt";
+        a_d = "a_des_"+date+".txt";
+        g_d = "g_des_"+date+".txt";
+
+        writeFile(a_m, a_medias.toString());
+        writeFile(a_d, a_desv_tipicas.toString());
+        writeFile(g_m, g_medias.toString());
+        writeFile(g_d, g_desv_tipicas.toString());
+
+    }
+
+    public void writeFile(String filename, String text){
+        //Crear fichero
+        File directory = this.getFilesDir();
+        File file = new File(directory, filename);
+        FileOutputStream outputStream;
+
+        //Escribir datos en el fichero
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            // Escritura del String de datos (String data) en el fichero de texto
+            outputStream.write(text.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "FILE created:\t" + directory + "/" + filename);
+    }
+
+
+
 }
